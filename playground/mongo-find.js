20a0bdaae71a33f4d1177184a902ada1c231666c
jdmurphy <jdmurphy@iastate.edu>
2018-06-22 19:03:48 -0500

    initial commit

---
+++ playground/mongo-find.js
@@ -0,0 +1,42 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB Server');
+  }
+  console.log('Connected successfully to MongoDB Server');
+
+  /*
+  db.collection('Todos').find({
+    _id: new ObjectID('5b2d88aef1676a1633754e74')
+  }).toArray().then((docs) => {
+    console.log('Todos:');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+  */
+
+  /*
+  db.collection('Todos').find().count().then((count) => {
+      console.log('Todos count: ' + count);
+    }, (err) => {
+      console.log('Unable to fetch todos', err);
+    });
+  */
+
+  db.collection('Users').find({
+    name: "Jack"
+  }).toArray().then((docs) => {
+    console.log('Users named Jack:');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+
+
+  //db.close();
+});
