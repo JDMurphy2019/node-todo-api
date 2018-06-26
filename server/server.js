c8b057cae5eea8d4e27e2e2e8ace14badbc17a13
jdmurphy <jdmurphy@iastate.edu>
2018-06-25 20:02:22 -0500

    Added GET /todo/id tests

--- server/server.js
+++ server/server.js
@@ -39,10 +39,10 @@ app.get('/todos/:id', (req, res) => {
     if(!todo){
       return res.status(404).send('');
     }
-    res.send({todos});
+    res.send({todo});
   }, (e) => {
     res.status(400).send('');
-  })
+  });
 });
 
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
+
 const expect = require('expect');
 const request = require('supertest');
-
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo.js');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -67,3 +70,19 @@ describe('GET /todos', () => {
       .end(done);
   });
 });
+
+  describe('GET /todos/:id', () => {
+    it('should return a todo doc', (done) => {
+      var todoID = todos[0]._id.toHexString();
+      var text = todos[0].text;
+      request(app)
+        .get('/todos/' + todoID)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.text).toBe(text);
+        })
+        .end(done);
+    });
+
+    //Should add error tests here...
+  });
